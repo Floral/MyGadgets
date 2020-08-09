3fe015702894cf853cdd58a04050ca9dd99baf38
DOA <qw.liang@outlook.com>
2020-08-09 17:58:13 +0800

    video2binary

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# MyGadgets
+# My Gadgets
+
 Here are some gadgets I wrote myself while working on other projects
+
+## Video2Binary
+
+这是一个将视频转成二进制文件的python小脚本（目前只支持转化为二值（其实自己修改一下也很简单））
+
+### 运行环境
+
+> python
+>
+> 需求额外安装库：
+>
+> - opencv-python (cv2)
+> - pillow (PIL)
+> - numpy
+
+### 使用方法
+
+把该脚本放在你所需要转化的视频所在的同一个目录下，在命令行中进入该目录，并使用python命令运行脚本，具体格式如下：
+
+`$ python video2binary.py "Your Video Name" "Your target binary file's name" [target img's width [target img's height] `
+
+后面两个参数为可选项，且为数字，前面两个文件名应放在英文的双引号中（包括后缀名）。
+
+最后生成的二进制文件与视频在同一个目录下，同时会在该目录下创建一个imgs文件夹，视频转化为的图片都放在其中。
---
+++ Video2Binary/video2binary.py
@@ -0,0 +1,77 @@
+import cv2
+import os
+from PIL import Image
+import numpy as np
+import sys
+
+frame_count = 0
+frame_num = 0
+frame_width = 0
+frame_height = 0
+
+def video2img(video_name, img_size = None):
+    global frame_count
+    global frame_num
+    global frame_width
+    global frame_height
+
+    cap = cv2.VideoCapture(video_name)
+    res = cap.isOpened()
+    frame_num = int(cap.get(7))      #视频总帧数
+    frame_width = int(cap.get(3))    #帧宽度
+    frame_height = int(cap.get(4))   #帧高度
+    path = './imgs' #图片保存在运行该脚本下的'./imgs'目录下
+    if not os.path.exists(path):
+        os.mkdir(path)
+    print('Total frames: ', frame_num)
+    print('frame width: ', frame_width, ' pixels')
+    print('frame height: ', frame_height, ' pixels')
+    print('Converting video into images...')
+
+    while res:
+        frame_count += 1
+        res, frame = cap.read()
+        #params = []
+        #params
+        if(not res):
+            print('Complete! ')
+            break
+        if(len(img_size) == 2): #如果指定了目标图像大小，则resize，否则保持默认
+            frame = cv2.resize(frame, (img_size),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite("./imgs/%d.png"%frame_count, frame)
+    cap.release()
+
+def img2bin(file_name):
+    global frame_num
+    
+    fp = open(file_name, 'wb+')  #以二进制格式写入，总是会创建一个新文件，从头开始写
+    print('Converting images to binary file...')
+    
+    for img_count in range(1,frame_num+1):
+        img = np.array(Image.open('./imgs/'+str(img_count)+'.png').convert('1'))  #convert转化为二值图像,0是黑,1是白
+        #img = np.array(Image.open(str(img_count)+'.png').convert('L'))  #convert转化为灰度图像
+        
+        img = img.ravel()   #把二维矩阵展开为一维数组，一行一行地拼接
+        #print(img)
+        fp.write(img)
+
+    fp.close()
+    print('Complete! ')
+
+if __name__ == "__main__":
+    video_name = sys.argv[1]        #原文件名
+    target_bin_name = sys.argv[2]   #生成的目标文件名
+    img_size = []   #目标图像大小
+    if(len(sys.argv) == 5): 
+        img_size.append(int(sys.argv[3]))
+        img_size.append(int(sys.argv[4]))
+    elif(len(sys.argv)>3 or len(sys.argv)<3):
+        raise RuntimeError("number of parameter error!")
+        
+    print("original video file name: ", video_name)
+    print("target binary file name: ", target_bin_name)
+    print("target image width: ", img_size[0] if(not len(img_size)) else "default")
+    print("target image height: ", img_size[1] if(not len(img_size)) else "default")
+
+    #video2img(video_name, tuple(img_size))
+    #img2bin(target_bin_name)
